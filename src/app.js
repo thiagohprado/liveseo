0822d145bbb2cc50e3e4b9430d55cbd5985768f8
Thiago Prado <32880994+thiagohprado@users.noreply.github.com>
2025-10-14 16:31:29 -0300

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,14 @@
+    // app.js
+    const express = require('express');
+    const app = express();
+    const PORT = process.env.PORT || 3001;
+
+    app.use(express.json()); // For parsing JSON request bodies
+
+    app.get('/', (req, res) => {
+      res.send('Hello LiveSEO from Express API !');
+    });
+
+    app.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
